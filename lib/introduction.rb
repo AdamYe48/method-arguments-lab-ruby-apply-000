4617113bacdb58d0aabc42700ac3e24fc8073abc
ADAM YE <adamye48@gmail.com>
2020-08-03 17:55:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 def introduction(dave)
   puts "Hi, my name is #{name}."
-def introduction_with_language(ember.js)
+def introduction_with_language(java)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
 end
